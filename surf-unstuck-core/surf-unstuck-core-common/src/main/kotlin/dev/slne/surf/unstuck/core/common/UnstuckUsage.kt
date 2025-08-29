4a455905d89d83a5682b15d4add98080d580f52b
Ammo <ammo@slne.dev>
2025-08-30 01:41:15 +0200

    feat: :sparkles: Enhance UnstuckUsage serialization with STREAM_CODEC and update version to 1.21.8-2.1.1-SNAPSHOT

--- surf-unstuck-core/surf-unstuck-core-common/src/main/kotlin/dev/slne/surf/unstuck/core/common/UnstuckUsage.kt
+++ surf-unstuck-core/surf-unstuck-core-common/src/main/kotlin/dev/slne/surf/unstuck/core/common/UnstuckUsage.kt
@@ -1,25 +1,47 @@
 package dev.slne.surf.unstuck.core.common
 
+import dev.slne.surf.cloud.api.common.netty.network.codec.streamCodec
+import dev.slne.surf.cloud.api.common.netty.protocol.buffer.SurfByteBuf
+import dev.slne.surf.cloud.api.common.netty.protocol.buffer.readEnum
 import dev.slne.surf.cloud.api.common.player.teleport.WorldLocation
 import dev.slne.surf.cloud.api.common.player.toOfflineCloudPlayer
 import dev.slne.surf.surfapi.core.api.messages.adventure.appendNewline
 import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
 import dev.slne.surf.surfapi.core.api.messages.builder.SurfComponentBuilder
-import kotlinx.serialization.Transient
+import kotlinx.serialization.Contextual
 import net.kyori.adventure.text.ComponentLike
 import java.time.ZonedDateTime
 import java.util.*
 
 data class UnstuckUsage(
-    val uuid: UUID,
-    val executedAt: ZonedDateTime,
+    val uuid: @Contextual UUID,
+    val executedAt: @Contextual ZonedDateTime,
     val server: String,
     val location: WorldLocation,
     var result: DbResult? = null,
-    var acknowledgedBy: UUID? = null,
+    var acknowledgedBy: @Contextual UUID? = null,
 ) {
-    @Transient
     val cloudPlayer get() = uuid.toOfflineCloudPlayer(false)
+
+    companion object {
+        val STREAM_CODEC = streamCodec<SurfByteBuf, UnstuckUsage>({ buf, usage ->
+            buf.writeUuid(usage.uuid)
+            buf.writeZonedDateTime(usage.executedAt)
+            buf.writeUtf(usage.server)
+            WorldLocation.STREAM_CODEC.encode(buf, usage.location)
+            buf.writeNullable(usage.result) { buf, r -> buf.writeEnum(r) }
+            buf.writeNullable(usage.acknowledgedBy) { buf, u -> buf.writeUuid(u) }
+        }, { buf ->
+            UnstuckUsage(
+                uuid = buf.readUuid(),
+                executedAt = buf.readZonedDateTime(),
+                server = buf.readUtf(),
+                location = WorldLocation.STREAM_CODEC.decode(buf),
+                result = buf.readNullable { it.readEnum<DbResult>() },
+                acknowledgedBy = buf.readNullable { it.readUuid() }
+            )
+        })
+    }
 
     enum class DbResult {
         SUCCESS,
--- surf-unstuck-core/surf-unstuck-core-common/src/main/kotlin/dev/slne/surf/unstuck/core/common/netty/protocol/serverbound/ServerboundCreateUnstuckUsagePacket.kt
+++ surf-unstuck-core/surf-unstuck-core-common/src/main/kotlin/dev/slne/surf/unstuck/core/common/netty/protocol/serverbound/ServerboundCreateUnstuckUsagePacket.kt
@@ -3,9 +3,26 @@ package dev.slne.surf.unstuck.core.common.netty.protocol.serverbound
 import dev.slne.surf.cloud.api.common.meta.SurfNettyPacket
 import dev.slne.surf.cloud.api.common.netty.network.protocol.PacketFlow
 import dev.slne.surf.cloud.api.common.netty.packet.NettyPacket
+import dev.slne.surf.cloud.api.common.netty.packet.packetCodec
+import dev.slne.surf.cloud.api.common.netty.protocol.buffer.SurfByteBuf
 import dev.slne.surf.unstuck.core.common.UnstuckUsage
 
 @SurfNettyPacket(id = "unstuck:log_unstuck_usage", flow = PacketFlow.SERVERBOUND)
 class ServerboundCreateUnstuckUsagePacket(
     val unstuckUsage: UnstuckUsage
-) : NettyPacket()
+) : NettyPacket() {
+
+    companion object {
+        val STREAM_CODEC = packetCodec(
+            ServerboundCreateUnstuckUsagePacket::write,
+            ::ServerboundCreateUnstuckUsagePacket
+        )
+    }
+
+    private constructor(buf: SurfByteBuf) : this(unstuckUsage = UnstuckUsage.STREAM_CODEC.decode(buf))
+
+    private fun write(buf: SurfByteBuf) {
+        UnstuckUsage.STREAM_CODEC.encode(buf, unstuckUsage)
+    }
+
+}
