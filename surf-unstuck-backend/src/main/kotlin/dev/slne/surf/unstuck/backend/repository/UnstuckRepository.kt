6075b37ad85870951c444bea9fcda340afd462fd
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-24 23:20:21 +0100

    refactor: Simplify `createUsage` with `suspendTransaction` in UnstuckRepository

--- surf-unstuck-backend/src/main/kotlin/dev/slne/surf/unstuck/backend/repository/UnstuckRepository.kt
+++ surf-unstuck-backend/src/main/kotlin/dev/slne/surf/unstuck/backend/repository/UnstuckRepository.kt
@@ -1,13 +1,14 @@
 package dev.slne.surf.unstuck.backend.repository
 
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.r2dbc.insert
+import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.r2dbc.transactions.suspendTransaction
 import dev.slne.surf.unstuck.backend.table.UnstuckUsagesTable
 import dev.slne.surf.unstuck.core.usage.UnstuckUsage
 
 val unstuckRepository = UnstuckRepository()
 
 class UnstuckRepository {
-    suspend fun createUsage(usage: UnstuckUsage) {
+    suspend fun createUsage(usage: UnstuckUsage) = suspendTransaction {
         UnstuckUsagesTable.insert {
             it[playerUuid] = usage.uuid
             it[result] = usage.result ?: error("Result is null")
